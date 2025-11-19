4f1f44629527394ec323bbc7728d82e7954e291e
Leonhard Breuer <git@leonhardbreuer.de>
2025-11-19 15:54:20 +0100

    feat: add CORSConfig struct

--- internal/domain/types.go
+++ internal/domain/types.go
@@ -3,3 +3,12 @@ package domain
 type WebhookData struct {
 	Ref string `json:"ref"`
 }
+
+type CORSConfig struct {
+	AllowedOrigins   []string
+	AllowedMethods   []string
+	AllowedHeaders   []string
+	ExposedHeaders   []string
+	MaxAge           int64
+	AllowCredentials bool
+}
