459680823146e299825abbfa8d7a480460c2f255
Leonhard Breuer <git@leonhardbreuer.de>
2025-11-20 08:01:30 +0100

    refactor: make public key only output when its not provided via env var

--- internal/helper/key.go
+++ internal/helper/key.go
@@ -3,12 +3,16 @@ package helper
 import (
 	"fmt"
 	"os"
+
+	"github.com/cheetahbyte/centra/internal/config"
 )
 
 func PrettyKey(pubKey string) {
-	fmt.Println("------------------------------------------------")
-	fmt.Println("Add this Deploy Key to your GitHub Repo:")
-	b, _ := os.ReadFile(pubKey)
-	fmt.Print(string(b))
-	fmt.Println("------------------------------------------------")
+	if config.GetPublicSSHKey() == "" {
+		fmt.Println("------------------------------------------------")
+		fmt.Println("Add this Deploy Key to your GitHub Repo:")
+		b, _ := os.ReadFile(pubKey)
+		fmt.Println(string(b))
+		fmt.Println("------------------------------------------------")
+	}
 }
